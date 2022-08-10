cce487258ee87a404299830edc65b4a28b926607
mwi33@protonmail.com <mwi33@proton.com>
2022-08-11 09:35:06 +1000

    changes to comply with pep-8 standard

--- pkg/__init__.py
+++ pkg/__init__.py
@@ -3,12 +3,19 @@ import pkg.main as main
 import pkg.wordlist as word
 import pkg.special_characters as characters
 
+
 @click.command()
-@click.option("--numberpasswords", default=10, help="How many passwords are to be created")
+@click.option(
+    "--numberpasswords", default=10, help="How many passwords are to be created"
+)
 @click.option("--words", default=4, help="Number of words to include")
 @click.option("--special", default=2, help="Number of special characters to include")
-@click.option("--exclude", default=" ", help="Special characters that should be excluded.  e.g @#")
-@click.option("--digits", default=2, help="Number of digits to be included.  Default is 2")
+@click.option(
+    "--exclude", default=" ", help="Special characters that should be excluded.  e.g @#"
+)
+@click.option(
+    "--digits", default=2, help="Number of digits to be included.  Default is 2"
+)
 def cli(words, special, exclude, digits, numberpasswords):
 
     word_list = word.dictionary
@@ -16,9 +23,11 @@ def cli(words, special, exclude, digits, numberpasswords):
 
     for password in range(numberpasswords):
 
-        password = main.construct_password(words, word_list, special, exclude, special_characters, digits)
+        password = main.construct_password(
+            words, word_list, special, exclude, special_characters, digits
+        )
 
-        entropy = main.calculate_entropy(special, exclude, digits,words)
+        entropy = main.calculate_entropy(special, exclude, digits, words)
 
         been_pwned = main.pwned(password)
 
